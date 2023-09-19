fed7969a103eb5868a611ffc03f45f3a2f825177
Almayo Ibrahim <almayo.online@gmail.com>
2023-09-19 11:25:03 +0300

    Initial Commit

---
+++ craftomiz/craftomizApp.swift
@@ -0,0 +1,17 @@
+//
+//  craftomizApp.swift
+//  craftomiz
+//
+//  Created by almayo ibrahim  on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct craftomizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
