71639f3d88f4cf6cfc306a0a587475e4c6bbd86e
Almayo Ibrahim <almayo.online@gmail.com>
2023-09-20 13:31:04 +0300

    Prepared TabBar design but still not connected to any pages

---
+++ craftomiz/View/HomePage/CategoriesGrid.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesGrid.swift
+//  craftomiz
+//
+//  Created by almayo ibrahim  on 20/09/2023.
+//
+
+import SwiftUI
+
+struct CategoriesGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoriesGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesGrid()
+    }
+}
---
+++ craftomiz/View/HomePage/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  craftomiz
+//
+//  Created by almayo ibrahim  on 20/09/2023.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
